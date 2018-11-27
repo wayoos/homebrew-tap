10fc18cfd20e6b313d1fdbb8f1a0833b510fd8ab
wayoosci <staff@wayoos.com>
2018-11-28 00:36:58 +0100

    Brew formula update for infra-compose version v0.2.1

--- Formula/infra-compose.rb
+++ Formula/infra-compose.rb
@@ -1,9 +1,9 @@
 class InfraCompose < Formula
   desc "Define and run multi-infrastructure tools."
   homepage "https://github.com/wayoos/infra-compose"
-  url "https://github.com/wayoos/infra-compose/releases/download/v0.2/infra-compose-darwin-amd64.tar.gz"
-  version "0.2"
-  sha256 "f95f11d942faab975fbaa5da0ca64eff6c37c3ec8473eb466169d3b43f4bc449"
+  url "https://github.com/wayoos/infra-compose/releases/download/v0.2.1/infra-compose-darwin-amd64.tar.gz"
+  version "0.2.1"
+  sha256 "ead28eb2cc87d379d43bc2c6ceaa9fb3898158c8cbd4db6efe5b1dcefc37ee3a"
 
   def install
     bin.install "infra-compose"
